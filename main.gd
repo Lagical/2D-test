1aee817a0fa12a9ba75d6f48ea8e032053aff18d
Lagical <70884073+Lagical@users.noreply.github.com>
2023-10-21 02:48:58 +0300

    2d demo done!

--- main.gd
+++ main.gd
@@ -18,6 +18,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -25,6 +27,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout():
