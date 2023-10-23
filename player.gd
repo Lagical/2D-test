343257a00af0c3f185fc7bb399d22627c7eb8f99
Lagical <70884073+Lagical@users.noreply.github.com>
2023-10-24 01:01:17 +0300

    dash progress

--- player.gd
+++ player.gd
@@ -6,14 +6,26 @@ const default_speed = 300.0
 const JUMP_VELOCITY = -400.0
 var jump_tracker = 0
 var multi_jump = 0
+var can_dash = false
+var dashing = false
+var direction = 0
+var dash_direction = 0
+var time_since_movement : float = 10
+var time_since_dash : float = 0
+var current_velocity = 0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+	
 
 func _physics_process(delta):
+	time_since_movement += delta
+	time_since_dash += delta
+	if time_since_dash > 0.5:
+		dashing = false
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle Jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,20 +40,33 @@ func _physics_process(delta):
 			pass
 	
 	if Input.is_action_pressed("jump") && not is_on_floor():
-		print(multi_jump)
-		print("toimi")
 		velocity.y = JUMP_VELOCITY
 		multi_jump = 0
 
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
+	direction = Input.get_axis("move_left", "move_right")
+		
+	if direction && dashing == false:
 		velocity.x = direction * SPEED
+		can_dash = true
+		time_since_movement = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	direction = Input.get_axis("move_left", "move_right")
+	
+	if direction && time_since_movement < 0.5 && can_dash == true:
+		time_since_dash = 0
+		can_dash = false
+		dashing = true
+		velocity.x = direction * 2000
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	#stops bunny hopping
 	if velocity.x != 0 && velocity.y == 0:
 		SPEED = default_speed
 	
+	#animation stuff
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
 	else:
