e8e59a9fdfc84315ed10bab8c47fbcb636d8c76e
Lagical <70884073+Lagical@users.noreply.github.com>
2023-10-23 03:46:00 +0300

    made bunny hop functionality

--- player.gd
+++ player.gd
@@ -1,33 +1,38 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED = 300.0
+const default_speed = 300.0
 const JUMP_VELOCITY = -400.0
+var jump_tracker = 0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_tracker += 1
+		print(velocity.length())
+		if velocity.length() > 400 && jump_tracker > 1:
+			SPEED += 25
+		elif velocity.length() == 400 && jump_tracker > 1:
+			print("toimi")
+			SPEED = default_speed
+			jump_tracker = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	"""if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1"""
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if velocity.x != 0 && velocity.y == 0:
+		SPEED = default_speed
 	
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
