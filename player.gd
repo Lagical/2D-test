d13e46daa587e85b10f7ed04903446cfdbdf7df6
Lagical <70884073+Lagical@users.noreply.github.com>
2023-10-28 02:33:17 +0300

    cleaned code bit and added wall jump and slide

--- player.gd
+++ player.gd
@@ -1,48 +1,46 @@
 extends CharacterBody2D
 
 
-var SPEED = 300.0
+var speed = 300.0
 const default_speed = 300.0
-const JUMP_VELOCITY = -400.0
+const jump_speed = -400.0
 var jump_tracker = 0
 var multi_jump = 0
+var dash_speed = 2500
 var dash = 0
 var can_dash = false
 var dashing = false
-var direction = 0
 var time_to_dash : float = 0
 var time_since_dash : float = 0
-var current_velocity = 0
+var time_to_bhop : float = 0
+var wall_sliding = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	walk_and_dash(delta)
+	jump(delta)
+	wall_slide(delta)
+	move_and_slide()
+	animations()
 	
-	print(SPEED)
+	
+		
+func walk_and_dash(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
 	
 	time_since_dash += delta
 	time_to_dash += delta
 	
-	if velocity.x == 0 && dashing == true:
+	"""if velocity.x == 0 && dashing == true:
 		dashing = false
 		can_dash = true
 		dash = 0
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	else:
-		multi_jump = 0
-	
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") && multi_jump < 2:
-		velocity.y = JUMP_VELOCITY
-		multi_jump += 1
-		if velocity.length() < 800 && is_on_floor():
-			SPEED += 25
-		
-	direction = Input.get_axis("move_left", "move_right")
-	
 	if Input.is_action_just_pressed("dash") && dash < 2:
 		dash += 1
 		can_dash = true
@@ -52,21 +50,52 @@ func _physics_process(delta):
 		dash = 0
 	
 	if dashing == false:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 		if Input.is_action_just_pressed("dash") && can_dash == true && dash == 2:
-			velocity.x = direction * 3000
+			velocity.x = direction * dash_speed
 			time_since_dash = 0
 			dashing = true
-			can_dash = false
+			can_dash = false""
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-		
-	#stops bunny hopping
-	if velocity.x != 0 && velocity.y == 0:
-		SPEED = default_speed
+		velocity.x = move_toward(velocity.x, 0, speed)"""
 	
-	#animation stuff
+	
+func jump(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
+			if velocity.length() < 800:
+				speed += 25
+		if is_on_wall() && Input.is_action_pressed("move_right"):
+			velocity.y = jump_speed
+			velocity.x = -2000
+		if is_on_wall() && Input.is_action_pressed("move_left"):
+			velocity.y = jump_speed
+			velocity.x = 2000
+	
+	#stops bunny hopping	
+	if velocity.x != 0 && velocity.y == 0:
+		speed = default_speed
+
+func wall_slide(delta):
+	if is_on_wall() && !is_on_floor():
+		if Input.is_action_pressed("move_left") || Input.is_action_pressed("ui_right"):
+			wall_sliding = true
+		else:
+			wall_sliding = false
+	else:
+		wall_sliding = false	
+	if wall_sliding:
+		velocity.y += (50 * delta)
+		velocity.y = min(velocity.y, 50)
+		print(velocity.y)
+	
+	
+	
+func animations():
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
 	else:
@@ -78,5 +107,3 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "jump"
-
-	move_and_slide()
